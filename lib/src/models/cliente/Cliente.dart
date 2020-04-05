1606a14d65dc5843776fed2c3a805d9772e636e2
Johnny Garcia <johnnygar98@hotmail.com>
2020-04-05 15:43:30 -0500

    Iniciando el proyecto movil

---
+++ lib/src/models/cliente/Cliente.dart
@@ -0,0 +1,38 @@
+class Cliente {
+
+  int id; 
+  int idUsuario;
+  String nombre;
+  String apellido; 
+  String identificacion;
+  String telefono;
+  int numCompras; 
+  DateTime ultimaCompra; 
+  double totalConsumos;
+
+  Cliente({
+    this.id,
+    this.idUsuario,
+    this.nombre,
+    this.apellido,
+    this.identificacion,
+    this.telefono,
+    this.numCompras,
+    this.ultimaCompra,
+    this.totalConsumos
+  });
+
+  Cliente.fromJSONMap(Map<String, dynamic> json) {
+    id = json['id'];
+    idUsuario = json['id_usuario'];
+    nombre = json['nombre'];
+    apellido = json['apellido'];
+    identificacion = json['identificacion'];
+    telefono = json['telefono'];
+    numCompras = json['num_compras'];
+    ultimaCompra = json['ultimaCompra']; 
+    totalConsumos = json['totalConsumos'];
+  }
+
+
+}
